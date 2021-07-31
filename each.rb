7e8c7069e8b5b9d160c867e0ea9314ef58a1f1db
Neylon Santos <neylonssantos@gmail.com>
2021-07-31 13:03:41 -0300

    Ajuste final

--- each.rb
+++ each.rb
@@ -1,26 +1,19 @@
 # each
-
 names = ['Joãozinho', 'Manoel', 'Juca']
-
 name = 'Leonardo Rocha'
 
 names.each do |name|
   puts name
-end;nil
+end
 
 puts name
 
-# inline
+# usando each inline
 names.each{ |name| puts name };nil
 
-# Usando hash
+# Usando each em hash
 posicoes = {1 => 'Primeiro', 2 => 'Segundo', 3 => 'Terceiro'}
 
 posicoes.each do |sapato, banana|
   puts "chave: #{sapato} valor: #{banana}"
 end
-
-.find_each
-
-[1, 2, 3, 4, 5]
-.each_with_index |element, index|
